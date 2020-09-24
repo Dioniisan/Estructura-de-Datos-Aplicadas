180651267d9bd91b1569571441e719f654075c82
Jesus Dionicio <misterioso2212mail.com>
2020-09-23 19:58:47 -0500

    MCD

--- Practica 2 - Recursividad/MCD.cs
+++ Practica 2 - Recursividad/MCD.cs
@@ -16,9 +16,32 @@ namespace Practica_2___Recursividad
         {
             InitializeComponent();
         }
+        public int Calcular(int n1, int n2)
+        {
+            int mcd;
+            if (n1 < n2)
+            {
+                
+                mcd = Calcular(n2, n1);
+            }
+            else if (n2 == 0)
+            {
+                mcd = n1;
+               
+            }
+            else
+            {
+                mcd = Calcular(n2, (n1 % n2));
+            }
 
+            return mcd;
+        }
         private void button1_Click(object sender, EventArgs e)
         {
+            int n1, n2;
+            n1 = Convert.ToInt32(textBox1.Text.Trim());
+            n2 = Convert.ToInt32(textBox2.Text.Trim());
+            label4.Text= Calcular(n1,n2).ToString();
 
         }
     }
